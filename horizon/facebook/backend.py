6351be8213e2ec7cd9e6e5c0406a2cd8a2cdaad0
Nachi Ueno <nachi@nttmcl.com>
2012-10-17 16:50:03 +0000

    Rewrite facebook auth module for folsom

--- horizon/facebook/backend.py
+++ horizon/facebook/backend.py
@@ -1,4 +1,6 @@
-import cgi, urllib, json
+import cgi
+import urllib
+import json
 
 from django.conf import settings
 from django.contrib.auth.models import User, AnonymousUser
@@ -14,16 +16,6 @@ from django.contrib import messages
 from openstack_auth.backend import KeystoneBackend
 
 
-def _set_session_data(request, token):
-    request.session['serviceCatalog'] = token.serviceCatalog
-    request.session['tenant'] = token.tenant['name']
-    request.session['tenant_id'] = token.tenant['id']
-    request.session['token'] = token.id
-    request.session['user_name'] = token.user['name']
-    request.session['user_id'] = token.user['id']
-    request.session['roles'] = token.user['roles']
-
-
 class FacebookBackend:
     def _admin_client(self):
         return  keystone_client.Client(username=settings.ADMIN_USER,
@@ -35,20 +27,24 @@ class FacebookBackend:
         """ Reads in a Facebook code and asks Facebook
             if it's valid and what user it points to. """
         keystone = KeystoneBackend()
+        self.keystone = keystone
         args = {
             'client_id': settings.FACEBOOK_APP_ID,
             'client_secret': settings.FACEBOOK_APP_SECRET,
-            'redirect_uri': request.build_absolute_uri('/facebook/authentication_callback'),
+            'redirect_uri': request.build_absolute_uri(
+                '/facebook/authentication_callback'),
             'code': token,
         }
-
         # Get a legit access token
-        target = urllib.urlopen('https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(args)).read()
+        target = urllib.urlopen(
+                'https://graph.facebook.com/oauth/access_token?'
+                + urllib.urlencode(args)).read()
         response = cgi.parse_qs(target)
         access_token = response['access_token'][-1]
 
         # Read the user's profile information
-        fb_profile = urllib.urlopen('https://graph.facebook.com/me?access_token=%s' % access_token)
+        fb_profile = urllib.urlopen(
+                'https://graph.facebook.com/me?access_token=%s' % access_token)
         fb_profile = json.load(fb_profile)
         tenant_id = None
         password = ""
@@ -56,7 +52,6 @@ class FacebookBackend:
             # Try and find existing user
             fb_user = FacebookProfile.objects.get(facebook_id=fb_profile['id'])
             user = fb_user.user
-
             # Update access_token
             fb_user.access_token = access_token
             password = fb_user.password
@@ -68,7 +63,6 @@ class FacebookBackend:
 
             facebook_id = fb_profile['id']
             username = "facebook%s" % facebook_id
-
             try:
                 user = User.objects.create_user(username, fb_profile['email'])
             except IntegrityError:
@@ -76,46 +70,64 @@ class FacebookBackend:
                 existing_user = User.objects.get(username=username)
                 existing_user.delete()
                 user = User.objects.create_user(username, fb_profile['email'])
-
             user.save()
 
-            password = "".join([random.choice(string.ascii_lowercase + string.digits) for i in range(8)])
+            password = "".join([random.choice(
+                                    string.ascii_lowercase + string.digits)
+                               for i in range(8)])
             # Create the FacebookProfile
-            fb_user = FacebookProfile(user=user, facebook_id=fb_profile['id'], access_token=access_token,password=password)
+            fb_user = FacebookProfile(user=user, facebook_id=fb_profile['id'],
+                                      access_token=access_token,
+                                      password=password)
             tenant_name = "facebook%s" % fb_profile['id']
             keystone_admin = self._admin_client()
 
-            tenant = keystone_admin.tenants.create(tenant_name,"Auto created account",True)
-            keystone_admin.users.create(tenant_name,password,fb_profile['email'],tenant.id,True)
+            tenant = keystone_admin.tenants.create(tenant_name,
+                                                   "Auto created account",
+                                                   True)
+            user = keystone_admin.users.create(tenant_name,
+                                               password,
+                                               fb_profile['email'],
+                                               tenant.id,
+                                               True)
+            admin_user_role = '193c1acbde854269a00386cff6ed16f2'
+            keystone_admin.roles.add_user_role(user.id,
+                                               admin_user_role,
+                                               tenant.id)
+
             fb_user.tenant_id = tenant.id
             tenant_id = fb_user.tenant_id
             fb_user.save()
-
         facebook_id = fb_profile['id']
         username = "facebook%s" % facebook_id
         try:
-            group_url = "https://graph.facebook.com/269238013145112/members?access_token=%s" % access_token
+            group_url = (
+                    "https://graph.facebook.com/"
+                    "269238013145112/members?access_token=%s"
+                    % access_token)
             f = urllib.urlopen(group_url)
             graph_data_json = f.read()
             f.close()
             graph_data = json.loads(graph_data_json)
             if len(graph_data['data']) > 0:
-                return keystone.authenticate(request=request,
+                user = keystone.authenticate(request=request,
                                       username=username,
                                       password=password,
                                       tenant=tenant_id,
                                       auth_url=settings.OPENSTACK_KEYSTONE_URL)
+                return user
             else:
-                messages.error(request, "Your facebookID is not in TryStack group yet.")
+                messages.error(
+                    request, "Your facebookID is not in TryStack group yet.")
         except Exception as e:
-            messages.error(request,"Failed to login facebookID %s" % e)
+            messages.error(
+                request, "Failed to login facebookID %s" % e)
 
     def get_user(self, user_id):
         """ Just returns the user of a given ID. """
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+        keystone = KeystoneBackend()
+        keystone.request = self.request
+        return keystone.get_user(user_id)
 
     supports_object_permissions = False
     supports_anonymous_user = True
--- horizon/facebook/views.py
+++ horizon/facebook/views.py
@@ -5,10 +5,12 @@ from django.conf import settings
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate
 from django.core.urlresolvers import reverse
+from openstack_auth.user import set_session_from_user, create_user_from_token
+from openstack_auth.forms import Login
+
 
 def login(request):
     """ First step of process, redirects user to facebook, which redirects to authentication_callback. """
-
     args = {
         'client_id': settings.FACEBOOK_APP_ID,
         'scope': settings.FACEBOOK_SCOPE,
@@ -16,25 +18,19 @@ def login(request):
     }
     return HttpResponseRedirect('https://www.facebook.com/dialog/oauth?' + urllib.urlencode(args))
 
+
 def authentication_callback(request):
     """ Second step of the login process.
     It reads in a code from Facebook, then redirects back to the home page. """
     code = request.GET.get('code')
     user = authenticate(token=code, request=request)
-
-    if user.is_anonymous():
-        #we have to set this user up
-        url = reverse('facebook_setup')
-        url += "?code=%s" % code
-
-        resp = HttpResponseRedirect(url)
-
-    else:
-        auth_login(request, user)
-
-        #figure out where to go after setup
-        url = getattr(settings, "LOGIN_REDIRECT_URL", "/")
-
-        resp = HttpResponseRedirect(url)
+    set_session_from_user(request, user)
+    auth_login(request, user)
+    region = request.user.endpoint
+    region_name = dict(Login.get_region_choices()).get(region)
+    request.session['region_endpoint'] = region
+    request.session['region_name'] = region_name
+    url = getattr(settings, "LOGIN_REDIRECT_URL", "/")
+    resp = HttpResponseRedirect(url)
 
     return resp
